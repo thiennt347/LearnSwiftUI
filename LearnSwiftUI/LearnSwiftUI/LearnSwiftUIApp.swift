5536334deb1dd21a4aa8ab1f1b3d8273439ea46b
Thien Nguyen <developer@goldfishcode.com>
2021-12-30 11:20:48 +0700

    init project

---
+++ LearnSwiftUI/LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Duong Pham on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
